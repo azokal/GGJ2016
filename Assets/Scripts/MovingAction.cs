30be822eacc41e5eb73bf88322d997c879f0bb6f
unknown <gamejam@D-34-52.bellecour.local>
2016-01-30 00:56:20 +0100

    script déplacement action

---
+++ Assets/Scripts/MovingAction.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingAction : MonoBehaviour {
+	public float vitesse = 100f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var t = this.transform.position;
+		t.x -= vitesse * Time.deltaTime;
+		this.transform.position	 = t;
+	}
+
+	void OnBecameInvisible() {
+		Destroy (gameObject);
+	}
+}
