dcc48a9ac342336ecd3213b17256a9c59be5dbd7
maxime constantinian <maxime.constantinian@gmail.com>
2016-01-30 02:09:57 +0100

    On fixe les ignores + validator.

--- Assets/Scripts/ValidatorScript.cs
+++ Assets/Scripts/ValidatorScript.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ValidatorScript : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 pos = new Vector2 (this.transform.position.x, this.transform.position.y);
+		var hits = Physics2D.OverlapCircleAll(pos, 0.1f);
+		print (hits);
+		foreach (var hit in hits) {
+			if (Input.GetKey(KeyCode.Space))
+				if (hit.name == "Action") {
+					Destroy(hit.gameObject);
+				}
+		}
+	}
+}
