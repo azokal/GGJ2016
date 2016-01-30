408d7106057c61f9b3e8d8d9766949a1593eab34
unknown <gamejam@D-34-62.bellecour.local>
2016-01-30 11:09:21 +0100

    changement de scène et pause

---
+++ Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchScene : MonoBehaviour {
+	public string  SceneName ;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void ToOtherScene ()
+	{
+		Application.LoadLevel(SceneName);
+	}
+}
