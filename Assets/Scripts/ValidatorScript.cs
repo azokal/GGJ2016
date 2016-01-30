3e4c64bfc6771a1b26621fec4ef4e89d884cc4c5
maxime constantinian <maxime.constantinian@gmail.com>
2016-01-30 03:35:28 +0100

    Ajout d'une gestion basic des inputs

--- Assets/Scripts/ValidatorScript.cs
+++ Assets/Scripts/ValidatorScript.cs
@@ -8,16 +8,101 @@ public class ValidatorScript : MonoBehaviour {
 	void Start () {
 		
 	}
+
+	string SimpleUpCheck() {
+		if (Input.GetKey(KeyCode.Alpha1))
+			return "SimpleUp";
+		return "";
+	}
+
+	string SimpleDownCheck() {
+		if (Input.GetKey(KeyCode.Alpha2))
+			return "SimpleDown";
+		return "";
+	}
+
+	string SimpleLeftCheck() {
+		if (Input.GetKey(KeyCode.Alpha3))
+			return "SimpleLeft";
+		return "";
+	}
 	
+	string SimpleRightCheck() {
+		if (Input.GetKey(KeyCode.Alpha4))
+			return "SimpleRight";
+		return "";
+	}
+
+	string DoubleUpCheck() {
+		if (Input.GetKey(KeyCode.Alpha5))
+			return "DoubleUp";
+		return "";
+	}
+	
+	string DoubleDownCheck() {
+		if (Input.GetKey(KeyCode.Alpha6))
+			return "DoubleDown";
+		return "";
+	}
+
+	string InvertUpCheck() {
+		if (Input.GetKey(KeyCode.Alpha7))
+			return "InvertUp";
+		return "";
+	}
+	
+	string InvertDownCheck() {
+		if (Input.GetKey(KeyCode.Alpha8))
+			return "InvertDown";
+		return "";
+	}
+
+	string TurnLeftCheck() {
+		if (Input.GetKey(KeyCode.Alpha9))
+			return "TurnLeft";
+		return "";
+	}
+	
+	string TurnRightCheck() {
+		if (Input.GetKey(KeyCode.Alpha0))
+			return "TurnRight";
+		return "";
+	}
+	
+	string CheckInput() {
+		var id = SimpleUpCheck ();
+		if (id == "")
+			id = SimpleDownCheck ();
+		if (id == "")
+			id = SimpleLeftCheck ();
+		if (id == "")
+			id = SimpleRightCheck ();
+		if (id == "")
+			id = DoubleUpCheck ();
+		if (id == "")
+			id = DoubleDownCheck ();
+		if (id == "")
+			id = InvertUpCheck ();
+		if (id == "")
+			id = InvertDownCheck ();
+		if (id == "")
+			id = TurnLeftCheck ();
+		if (id == "")
+			id = TurnRightCheck ();
+		return id;
+	}
+
 	// Update is called once per frame
 	void Update () {
 		Vector2 pos = new Vector2 (this.transform.position.x, this.transform.position.y);
 		var hits = Physics2D.OverlapCircleAll(pos, 0.1f);
-		print (hits);
 		foreach (var hit in hits) {
-			if (Input.GetKey(KeyCode.Space))
 				if (hit.name == "Action") {
-					Destroy(hit.gameObject);
+					var ci = CheckInput();
+					if (ci != "") {
+						if (ci == hit.gameObject.GetComponent<ActionType>().eventType)
+						Destroy(hit.gameObject);
+					}
 				}
 		}
 	}
