309cb6927346556be31a05e8d5f0be2263265f45
SlowFlo <fbautry@gmail.com>
2016-01-30 01:48:42 +0100

    début spawn action

--- Assets/Scripts/SpawnAction.cs
+++ Assets/Scripts/SpawnAction.cs
@@ -2,14 +2,18 @@
 using System.Collections;
 
 public class SpawnAction : MonoBehaviour {
+	public GameObject action;
+	public float interval;
+	private float timer;
 
 	// Use this for initialization
 	void Start () {
-	
+		timer = 0f;
+		//Instantiate(action, this.transform.position, Quaternion.identity);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 }
