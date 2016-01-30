e694364011482eaffdbfbd9829213a888e51d512
SlowFlo <fbautry@gmail.com>
2016-01-30 11:54:17 +0100

    intervals de spawn des actions selon la difficulté

--- Assets/Scripts/SpawnAction.cs
+++ Assets/Scripts/SpawnAction.cs
@@ -7,41 +7,50 @@ public class SpawnAction : MonoBehaviour {
 	public GameObject[] actionsDifficiles;
 	private GameObject[] actionsActuelles;
 
-	public float interval = 1f;
 	private float timer = 0f;
 
 	public enum enDifficulte{facile,moyen,difficile};
 	public enDifficulte difficulte = enDifficulte.facile;
+
 	public float vitesseFacile = 5f;
 	public float vitesseMoyen = 10f;
 	public float vitesseDifficile= 15f;
 
+	public float intervalFacile = 1f;
+	public float intervalMoyen = 0.5f;
+	public float intervalDifficile = 0.3f;
+	private float intervalActuel;
+
 	// Use this for initialization
 	void Start () {
 		actionsActuelles = actionsFaciles;
+		intervalActuel = intervalFacile;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		switch (difficulte) {
 		case enDifficulte.facile:
-			MovingAction.vitesse = 5f;
+			MovingAction.vitesse = vitesseFacile;
 			actionsActuelles = actionsFaciles;
+			intervalActuel = intervalFacile;
 			break;
 		case enDifficulte.moyen:
-			MovingAction.vitesse = 10f;
+			MovingAction.vitesse = vitesseMoyen;
 			actionsActuelles = actionsMoyennes;
+			intervalActuel = intervalMoyen;
 			break;
 		case enDifficulte.difficile:
-			MovingAction.vitesse = 15f;
+			MovingAction.vitesse = vitesseDifficile;
 			actionsActuelles = actionsDifficiles;
+			intervalActuel = intervalDifficile;
 			break;
 		}
 
 		if (timer <= 0f) {
 			var o = Instantiate (actionsActuelles [Random.Range (0, actionsActuelles.Length)], this.transform.position, Quaternion.identity);
 			o.name = "Action";
-			timer = interval;
+			timer = intervalActuel;
 		}
 
 		timer -= Time.deltaTime;
