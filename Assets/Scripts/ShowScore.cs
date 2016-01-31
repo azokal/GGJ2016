4d1880895d9dac563139663b82650608c2df5e4b
unknown <gamejam@D-34-61.bellecour.local>
2016-01-31 11:22:47 +0100

    Début de score foireux

--- Assets/Scripts/ShowScore.cs
+++ Assets/Scripts/ShowScore.cs
@@ -5,16 +5,19 @@ using UnityEngine.UI;
 public class ShowScore : MonoBehaviour {
 	private Text scoreText; 
 	public GameObject validator;
+	public GameObject fond;
 
 	// Use this for initialization
 	void Start () {
+
 		scoreText = GetComponent<UnityEngine.UI.Text>();
+	//	scoreText.transform.position = 
 	
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		scoreText.text =("Score : "+ validator.GetComponent<ValidatorScript>().score +"\n Combo : "+ validator.GetComponent<ValidatorScript>().serie + "\nMultiplicateur : x"+ validator.GetComponent<ValidatorScript>().combo);
+		scoreText.text =("Score : "+ validator.GetComponent<ValidatorScript>().score +"\nCombo : "+ validator.GetComponent<ValidatorScript>().serie + "\nMultiplicateur : x"+ validator.GetComponent<ValidatorScript>().combo);
 	}
 }
