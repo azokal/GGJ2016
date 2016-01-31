a460a7aa797ed0f5338dc253b80dca695fafad8b
unknown <gamejam@D-34-61.bellecour.local>
2016-01-31 16:17:59 +0100

    Score ok

--- Assets/Scripts/ShowScore.cs
+++ Assets/Scripts/ShowScore.cs
@@ -5,20 +5,23 @@ using UnityEngine.UI;
 public class ShowScore : MonoBehaviour {
 	private Text scoreText; 
 	public GameObject validator;
-	public GameObject fond;
 
 	// Use this for initialization
 	void Start () {
-
 		scoreText = GetComponent<UnityEngine.UI.Text>();
-	//	fond.transform.position = 
-	//	scoreText.transform.position = 
-	
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		scoreText.text =("Score : "+ validator.GetComponent<ValidatorScript>().score +"\nCombo : "+ validator.GetComponent<ValidatorScript>().serie);
+		if (scoreText.gameObject.name == "Score") {
+			scoreText.text = ("Score : " + validator.GetComponent<ValidatorScript> ().score);
+		}
+		if (scoreText.gameObject.name == "Multiplicateur") {
+			scoreText.text = ("x" + validator.GetComponent<ValidatorScript> ().combo);
+		}
+		if (scoreText.gameObject.name == "Combo") {
+			scoreText.text = ("Combo : " + validator.GetComponent<ValidatorScript> ().serie);
+		}
 	}
 }
